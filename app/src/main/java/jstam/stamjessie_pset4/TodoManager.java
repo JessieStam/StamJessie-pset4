df5d6c07e4460d021bec3eefead1377546fb10e9
JessieStam <jessiestam@quicknet.nl>
2016-05-11 16:41:57 +0200

    Second commit

---
+++ app/src/main/java/jstam/stamjessie_pset4/TodoManager.java
@@ -0,0 +1,21 @@
+package jstam.stamjessie_pset4;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Jessie on 11/05/2016.
+ */
+public class TodoManager extends SQLiteOpenHelper {
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
