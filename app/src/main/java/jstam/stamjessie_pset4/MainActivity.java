60cf20f8cf985db62476ba331f2816b8ab956daf
JessieStam <jessiestam@quicknet.nl>
2016-05-12 22:18:45 +0200

    Third commit

--- app/src/main/java/jstam/stamjessie_pset4/MainActivity.java
+++ app/src/main/java/jstam/stamjessie_pset4/MainActivity.java
@@ -1,13 +1,94 @@
 package jstam.stamjessie_pset4;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todo_list_list;
+    ListView screen_list;
+    ArrayAdapter<String> todoAdapter;
+    EditText user_input;
+    String todo_title;
+    TodoManager TodoManager = new TodoManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user_input = (EditText) findViewById(R.id.user_input);
+        screen_list = new ListView(this);
+        screen_list = (ListView) findViewById(R.id.titleList);
+        todoAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_list_item_1, todo_list_list);
+
+        /*
+         * set onclick listener for ListView items to check/uncheck them
+         */
+        screen_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // ga naar een nieuwe activity
+                Intent listItems = new Intent(this, SecondActivity.class);
+                startActivity(listItems);
+
+                // Jessie, kijk er nog even naar of je dit moet afsluiten of niet
+                finish();
+            }
+        });
+
+        /*
+         * set long click listener for removing items
+         */
+        screen_list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View string, int position, long id) {
+
+                // remove the item at the touched position and update data
+                todo_list_list.remove(position);
+                todoAdapter.notifyDataSetChanged();
+
+                //remove title from the SQLite
+                TodoManager.delete(TodoItem.id);
+
+                return true;
+            }
+        });
+    }
+
+    /*
+    * Adds an item to the list
+    */
+    public void addToList(View view) {
+
+        // use adapter to put todo_list information to screen_list
+        screen_list.setAdapter(todoAdapter);
+
+        todo_title = user_input.getText().toString();
+
+        TodoItem new_title = new TodoItem(todo_title);
+
+        // add user input to ListView
+        todo_list_list.add(new_title);
+
+        // refresh ListView
+        todoAdapter.notifyDataSetChanged();
+
+        // add title to SQLite
+        TodoManager.create(todo_title);
+
+        // clear the input line after text is added
+        user_input.getText().clear();
     }
 }
--- app/src/main/java/jstam/stamjessie_pset4/TodoManager.java
+++ app/src/main/java/jstam/stamjessie_pset4/TodoManager.java
@@ -1,21 +1,113 @@
 package jstam.stamjessie_pset4;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Jessie on 11/05/2016.
  */
 public class TodoManager extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "firstdb.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE = "todo_table";
+    public TodoManager(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION); }
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+        // create the table, add id and to-do items
+        String query = "CREATE TABLE"+ TABLE+ " (_id"+" INTEGER PRIMARY KEY AUTOINCREMENT" + "todo_text TEXT)";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        // drop table and create it again when application is updated
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE);
+        onCreate(db);
+    }
+
+    /*
+     * Add to-do items to the list
+     */
+    public void create(String todo_name) {
+
+        // initialize database for writing
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // add to-do item to the list and insert into table
+        values.put("todo_text", todo_name);
+        db.insert(TABLE, null, values);
+        db.close();
+    }
+
+    /*
+     * Read through the database
+     */
+    public ArrayList<HashMap<String, String>> read() {
+
+        // initialize database for reading
+        SQLiteDatabase db = getReadableDatabase();
+
+        // select id and item from the table
+        String query = "SELECT _id "+"todo_text "+ " FROM"+ TABLE;
+        ArrayList<HashMap<String, String>> todo = new ArrayList<>();
+        Cursor cursor = db.rawQuery(query, null);
+
+        // for every item in the list, read id and to-do
+        if (cursor.moveToFirst()) {
+            do {
+                HashMap<String, String> todo_list = new HashMap<>();
+                todo_list.put("id", cursor.getString(cursor.getColumnIndex(" id")));
+                todo_list.put("todo_item", cursor.getString
+                        (cursor.getColumnIndex("todo")));
+                todo.add(todo_list);
+            }
+            while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return todo;
+    }
+
+    /*
+     * Update database
+     */
+    public void update(TodoItem todo_title) {
+
+        // initialize database for writing
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // add to-do item to list and add to the table
+        values.put("todo", TodoItem.getTitle());
+
+        // hoe vul ik deze id in?
+        db.update(TABLE, values, " id = ? ", new String[] {String.valueOf(TodoItem.id)});
+        db.close();
+    }
+
+    /*
+     * Delete item from the table
+     */
+    public void delete(int id) {
+
+        // initialize database for writing
+        SQLiteDatabase db = getWritableDatabase();
+
+        // delete to-do item from the table
+        db.delete(TABLE, " _id = ? ", new String[] {String.valueOf(id)});
+        db.close();
     }
 }
